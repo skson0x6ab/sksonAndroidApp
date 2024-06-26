01e4a823304910405e71492acd9a9fa562b9d9fb
skson0x6ab <skson0x6ab@gmail.com>
2024-06-26 17:18:10 +0900

    json upload 테스트 완료

--- app/src/main/java/com/skson0x6ab/sksonandroidapp/MainActivity.kt
+++ app/src/main/java/com/skson0x6ab/sksonandroidapp/MainActivity.kt
@@ -16,6 +16,7 @@ import java.util.Base64
 import okhttp3.MediaType.Companion.toMediaType
 import android.util.Log
 import android.widget.TextView
+import org.json.JSONArray
 
 class MainActivity : ComponentActivity() {
 
@@ -40,40 +41,41 @@ class MainActivity : ComponentActivity() {
             textview1.text = ""
             val url = "https://www.ff14.co.kr/news/notice?category=3"
             val jsonName = "FF14.json"
-            val parsingRule = "table.ff14_board_list tbody tr"
-            fetchWebPage(url, jsonName, parsingRule)
+            val masterParsingRule = "table.ff14_board_list tbody tr"
+            val subParsingRule = arrayListOf("td.type", "td .title a", "td.date")
+            val subItem = arrayListOf("category", "text", "date")
+            fetchWebPage(url, jsonName, masterParsingRule, subParsingRule, subItem)
         }
         button2.setOnClickListener {
             textview1.text = ""
-            val url = ""
+            val url = "https://maplestory.nexon.com/News/Update"
             val jsonName = "Maplestory.json"
-            val parsingRule = ""
-            fetchWebPage(url, jsonName, parsingRule)
+            val masterParsingRule = "div.update_board ul li"
+            val subParsingRule = arrayListOf("empty","p a span", ".heart_date dl dd")
+            val subItem = arrayListOf("category", "text", "date")
+            fetchWebPage(url, jsonName, masterParsingRule, subParsingRule, subItem)
         }
         button3.setOnClickListener {
             textview1.text = ""
-            val url = ""
+            val url = "https://genshin.hoyoverse.com/m/ko/news/"
             val jsonName = "Genshin.json"
-            val parsingRule = ""
-            fetchWebPage(url, jsonName, parsingRule)
+            val masterParsingRule = "script"
+            val subParsingRule = arrayListOf(",\"2024-",",")
+            val subItem = arrayListOf("category", "text", "date")
+            fetchWebPageNuxtGenshin(url, jsonName, masterParsingRule, subParsingRule, subItem)
         }
         button4.setOnClickListener {
             textview1.text = ""
-            val url = ""
+            val url = "https://hsr.hoyoverse.com/ko-kr/news"
             val jsonName = "StarRail.json"
-            val parsingRule = ""
-            fetchWebPage(url, jsonName, parsingRule)
-        }
-        button5.setOnClickListener {
-            textview1.text = ""
-            val url = ""
-            val jsonName = "TheKingdomOfWinds.json"
-            val parsingRule = ""
-            fetchWebPage(url, jsonName, parsingRule)
+            val masterParsingRule = "script"
+            val subParsingRule = arrayListOf("sTitle:","\"","dtStartTime:")
+            val subItem = arrayListOf("category", "text", "date")
+            fetchWebPageNuxtStarRail(url, jsonName, masterParsingRule, subParsingRule, subItem)
         }
     }
 
-    private fun fetchWebPage(url: String, jsonName: String, parsingRule: String) {
+    private fun fetchWebPage(url: String, jsonName: String, masterParsingRule: String, subParsingRule: ArrayList<String>, subItem: ArrayList<String>) {
         // 비동기적으로 네트워크 작업 수행
         CoroutineScope(Dispatchers.IO).launch {
             try {
@@ -92,16 +94,160 @@ class MainActivity : ComponentActivity() {
                     val document: Document = Jsoup.parse(responseBody)
 
                     // 데이터 파싱
-                    val data = document.select(parsingRule)
+                    val datas = document.select(masterParsingRule)
 
                     // JSON 객체 생성
-                    val jsonObject = JSONObject()
-                    jsonObject.put("data", data)
+                    //var jsonObject = JSONObject()
+                    var jsonArray = JSONArray()
+
+                    var num = 1
+                    for(data in datas){
+                        Log.i(TAG, data.toString())
+                        val tmpJsonObject = JSONObject()
+                        for(i in 0 until subItem.size){
+                            val tmpData =
+                                if (subParsingRule[i] == "empty"){
+                                    num.toString()
+
+                                }else{
+                                    data.select(subParsingRule[i]).text()
+                                }
+                            tmpJsonObject.put(subItem[i], tmpData)
+                        }
+                        num += 1
+                        jsonArray.put(tmpJsonObject)
+                    }
 
                     // JSON 문자열로 변환
-                    val jsonString = jsonObject.toString()
+                    val jsonString = jsonArray.toString()
                     Log.i(TAG, jsonString)
 
+                    // JSON 파일로 저장
+                    val jsonFile = File(filesDir, jsonName)
+                    jsonFile.writeText(jsonString)
+
+                    // GitHub에 업로드
+                    uploadToGitHub(jsonString, jsonName)
+                } else {
+                    println("Failed to fetch the webpage.")
+                    textview1.append("Failed to fetch the webpage.\n")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun fetchWebPageNuxtStarRail(url: String, jsonName: String, masterParsingRule: String, subParsingRule: ArrayList<String>, subItem: ArrayList<String>) {
+        // 비동기적으로 네트워크 작업 수행
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                // OkHttp를 사용해 HTTP 요청 보내기
+                val request = Request.Builder().url(url).build()
+                Log.i(TAG, "HTTP Request")
+                textview1.append("HTTP Request\n")
+
+                val response = client.newCall(request).execute()
+                Log.i(TAG, "HTTP Response")
+                textview1.append("HTTP Response\n")
+
+                // 응답이 성공적이면
+                if (response.isSuccessful) {
+                    val responseBody = response.body?.string() ?: ""
+                    val document: Document = Jsoup.parse(responseBody)
+
+                    // 데이터 파싱
+                    val datas = document.select(masterParsingRule).toString()
+
+                    val parts = datas.split(subParsingRule[0])
+                    // JSON 객체 생성
+                    //var jsonObject = JSONObject()
+                    var jsonArray = JSONArray()
+
+                    for(j in 2 until parts.size){
+                        val tmpJsonObject = JSONObject()
+
+                        var tmpString = parts[j].split(subParsingRule[1])
+
+                        if(tmpString[1].length <=7 ){
+                            break
+                        }
+                        val text1 = tmpString[1].replace("\"","")
+
+                        var tmpString2 = parts[j].split(subParsingRule[2])
+                        val timestamp1 = tmpString2[1].split(subParsingRule[1])
+                        val timestamp2 = timestamp1[1].replace("\"","")
+
+                        tmpJsonObject.put(subItem[0], j-1)
+                        tmpJsonObject.put(subItem[1], text1)
+                        tmpJsonObject.put(subItem[2], timestamp2)
+
+                        jsonArray.put(tmpJsonObject)
+                    }
+
+                    // JSON 문자열로 변환
+                    val jsonString = jsonArray.toString()
+                    Log.i(TAG, jsonString)
+
+                    // JSON 파일로 저장
+                    val jsonFile = File(filesDir, jsonName)
+                    jsonFile.writeText(jsonString)
+
+                    // GitHub에 업로드
+                    uploadToGitHub(jsonString, jsonName)
+                } else {
+                    println("Failed to fetch the webpage.")
+                    textview1.append("Failed to fetch the webpage.\n")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun fetchWebPageNuxtGenshin(url: String, jsonName: String, masterParsingRule: String, subParsingRule: ArrayList<String>, subItem: ArrayList<String>) {
+        // 비동기적으로 네트워크 작업 수행
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                // OkHttp를 사용해 HTTP 요청 보내기
+                val request = Request.Builder().url(url).build()
+                Log.i(TAG, "HTTP Request")
+                textview1.append("HTTP Request\n")
+
+                val response = client.newCall(request).execute()
+                Log.i(TAG, "HTTP Response")
+                textview1.append("HTTP Response\n")
+
+                // 응답이 성공적이면
+                if (response.isSuccessful) {
+                    val responseBody = response.body?.string() ?: ""
+                    val document: Document = Jsoup.parse(responseBody)
+
+                    // 데이터 파싱
+                    val datas = document.select(masterParsingRule).toString()
+
+                    val parts = datas.split(subParsingRule[0])
+                    // JSON 객체 생성
+                    //var jsonObject = JSONObject()
+                    var jsonArray = JSONArray()
+
+                    for(j in 1 until parts.size){
+                        val tmpJsonObject = JSONObject()
+
+                        var tmpString = parts[j].split(subParsingRule[1])
+                        val timestamp1 = "2024-" + tmpString[0].replace("\"","")
+                        val text1 = tmpString[1].replace("\"","")
+
+                        tmpJsonObject.put(subItem[0], j)
+                        tmpJsonObject.put(subItem[1], text1)
+                        tmpJsonObject.put(subItem[2], timestamp1)
+
+                        jsonArray.put(tmpJsonObject)
+                    }
+
+                    // JSON 문자열로 변환
+                    val jsonString = jsonArray.toString()
+                    Log.i(TAG, jsonString)
 
                     // JSON 파일로 저장
                     val jsonFile = File(filesDir, jsonName)
@@ -120,7 +266,7 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun uploadToGitHub(jsonString: String, jsonName: String) {
-        val token = "보안키"  // 적절한 권한을 가진 토큰
+        val token = "토큰값 입력 필요"  // 적절한 권한을 가진 토큰
         val username = "skson0x6ab"
         val repo = "DataRepository"
         val branch = "main"
